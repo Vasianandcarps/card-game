b2e4b0675fbe31b7320234dc1719f0da97b9cc58
Vasianandcarps <90087480+Vasianandcarps@users.noreply.github.com>
2021-12-12 12:34:51 +0200

    Update queen.js

--- card game/queen.js
+++ card game/queen.js
@@ -1,5 +1,5 @@
 let values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
-
+let arr1 = [];
 function shuffle(arr) {
   let rand, temp;
   for (let i = 0; i < arr.length; i++) {
@@ -14,29 +14,35 @@ shuffle(values);
 alert(values);
 
 function play() {
-    let turn = 0;
-    let card;
-    while (values.length>0){
-      turn++;
-      if(turn%2==1){
-        alert("You pick card!")
-        card = values.pop();
-        alert(card);
-      } else { 
-        alert("I pick card!")
-        card = values.pop();
-      
-      }
+  let turn = 0;
+  let card;
+  while (values.length > 0) {
+    turn++;
+    if (turn % 2 == 1) {
+      alert("You pick card!");
+      card = values.pop();
+      arr1.push(card);
       alert(card);
-      if(card == "Q" ){
-            if(turn%2==1){
-              alert("You win");
-              break;
-            }else {
-              alert("I win")
-              break;
-            }
+    } else {
+      alert("I pick card!");
+      card = values.pop();
+      arr1.push(card);
+      alert(card);
+    }
+    alert(card);
+    if (card == "Q") {
+      if (turn % 2 == 1) {
+        alert("You win");
+        break;
+      } else {
+        alert("I win");
+        break;
       }
     }
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    document.write(arr1[i]);
+  }
 }
 play(values);
+
